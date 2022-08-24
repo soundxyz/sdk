95a7b59d102bea7617ddf7c8b5a7891753c16410
Matt Masurka <m.masurka@gmail.com>
2022-08-23 19:37:13 -0700

    Adds vitest

---
+++ packages/sdk/test/index.test.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+
+import { describe, expect, it } from 'vitest'
+import { SoundClient } from '../src/index'
+import { StaticJsonRpcProvider } from '@ethersproject/providers'
+
+const providerURL =
+  process.env.TEST_ENV === 'local'
+    ? 'http://localhost:8545'
+    : `https://eth-goerli.alchemyapi.io/v2/${process.env.NEXT_PUBLIC_ALCHEMY_KEY}`
+
+const provider = new StaticJsonRpcProvider({ url: providerURL })
+const client = new SoundClient({ chainId: 5, provider })
+
+describe('isSoundEdition', () => {
+  it("Should throw error if the address isn't valid", async () => {
+    await expect(() => client.isSoundEdition('0x123')).rejects.toThrowError('Invalid contract address')
+  })
+})
