9915a889daeacc642205e0bb1aaedd0fce7ba86d
Matt Masurka <m.masurka@gmail.com>
2022-08-29 13:50:23 -0700

    merkle test passing

--- packages/sdk/test/dummyData.ts
+++ packages/sdk/test/dummyData.ts
@@ -1,6 +1,7 @@
 import { MerkleTree } from 'merkletreejs'
 import { sha256 } from '@ethersproject/sha2'
 
+// First 3 addresses of mnemonic: 'test test test ...'
 const recipients = [
   '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
   '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
@@ -8,15 +9,10 @@ const recipients = [
 ]
 
 const tree = new MerkleTree(recipients, sha256)
-const root = tree.getRoot().toString('hex')
-// const leaf = recipients[2]
-// const proof = tree.getProof(leaf)
-// console.log('validProof?', tree.verify(proof, leaf, root)) // true
-// const badLeaf = recipients[5]
-// const badProof = tree.getProof(badLeaf)
-// // console.log('validProof?', merkleTree.verify(badProof, badLeaf, merkleRootHash)) // false
+const root = '0x' + tree.getRoot().toString('hex')
 
-export const dummyMerkle = {
+export const dummyMerkleDrop = {
   recipients,
   tree,
+  root,
 }
