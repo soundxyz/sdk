c7a614c37acd7c2ab4ee250c61fcd5cf125de99e
Matt Masurka <m.masurka@gmail.com>
2022-08-29 17:21:09 -0700

    Updates interfaceIds & sound-protocol package

--- packages/sdk/src/config.ts
+++ packages/sdk/src/config.ts
@@ -23,14 +23,17 @@ export const chainIdToInfo: {
   },
 }
 
+const rangeIface = RangeEditionMinter__factory.createInterface()
+// const rangeId = rangeIface
+
 export const UINT32_MAX = 4294967295
 
 export const interfaceIds = {
   ISoundEditionV1: '0x41f5b0cb',
-  IMinterModule: '0x97656fd6',
-  IFixedPriceSignatureMinter: '0xc7843744',
-  IMerkleDropMinter: '0xd73b07f1',
-  IRangeEditionMinter: '0xfd5ca5c9',
+  IMinterModule: '0x84634360',
+  IFixedPriceSignatureMinter: '0x81ca70f4',
+  IMerkleDropMinter: '0x84b6980c',
+  IRangeEditionMinter: '0xc73d6ffa',
 }
 
 export const minterFactoryMap = {
