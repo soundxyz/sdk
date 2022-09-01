0701e0ffcf6a1e615d5bb5a6aca39ad524519a1a
Pablo Sáez <pablosaez1995@gmail.com>
2022-09-01 10:24:03 -0400

    refactor build script

--- packages/sdk/build.mjs
+++ packages/sdk/build.mjs
@@ -1,74 +1,85 @@
 // @ts-check
 
 import { buildCode } from 'bob-ts'
-import { execSync } from 'child_process'
+import { execSync, spawn } from 'child_process'
 import { writeFile, readFile } from 'fs/promises'
 
 execSync('pnpm graphql-codegen', {
   stdio: 'inherit',
 })
 
-await buildCode({
-  entryPoints: ['src'],
-  clean: true,
-  format: 'interop',
-  outDir: 'dist',
-  target: 'es2020',
-  sourcemap: false,
+spawn(`pnpm tsc -p tsconfig.build.json`, {
+  stdio: 'inherit',
+  shell: true,
 })
+
+const [pkgString] = await Promise.all([
+  readFile('package.json', 'utf-8'),
+  buildCode({
+    entryPoints: ['src'],
+    clean: true,
+    format: 'interop',
+    outDir: 'dist',
+    target: 'es2020',
+    sourcemap: false,
+  }),
+])
+
+/**
+ * @type {{name: string, version: string, exports: Record<string,{types: string; require: string; import: string} | string>; main: string; module: string; types: string; sideEffects: false; author: string, license: string, dependencies: Record<string,string>}}
+ */
+const {
+  name,
+  version,
+  exports: pkgExports,
+  main,
+  module: modulePkg,
+  types,
+  sideEffects,
+  author,
+  dependencies,
+  license,
+} = JSON.parse(pkgString)
+
+const exports = {}
+
+function cleanDistPrefix(
+  /**
+   * @type {string}
+   */
+  path,
+) {
+  return path.replace('./dist', '.')
+}
+
+for (const [key, value] of Object.entries(pkgExports)) {
+  if (typeof value === 'string') {
+    exports[key] = cleanDistPrefix(value)
+  } else {
+    exports[key] = {
+      types: cleanDistPrefix(value.types),
+      require: cleanDistPrefix(value.require),
+      import: cleanDistPrefix(value.import),
+    }
+  }
+}
 
 await writeFile(
   'dist/package.json',
-  await readFile('package.json', 'utf-8').then(async (pkgString) => {
-    /**
-     * @type {{name: string, version: string, exports: Record<string,{types: string; require: string; import: string} | string>; main: string; module: string; types: string; sideEffects: false; author: string, license: string, dependencies: Record<string,string>}}
-     */
-    const {
+  JSON.stringify(
+    {
       name,
       version,
-      exports: pkgExports,
-      main,
-      module: modulePkg,
-      types,
+      main: cleanDistPrefix(main),
+      module: cleanDistPrefix(modulePkg),
+      types: cleanDistPrefix(types),
+      exports,
       sideEffects,
       author,
-      dependencies,
       license,
-    } = JSON.parse(pkgString)
-
-    const exports = {}
-
-    for (const [key, value] of Object.entries(pkgExports)) {
-      if (typeof value === 'string') {
-        exports[key] = value.replace('./dist', '.')
-      } else {
-        exports[key] = {
-          types: value.types.replace('./dist', '.'),
-          require: value.require.replace('./dist', '.'),
-          import: value.import.replace('./dist', '.'),
-        }
-      }
-    }
-
-    return JSON.stringify(
-      {
-        name,
-        version,
-        main: main.replace('./dist', '.'),
-        module: modulePkg.replace('./dist', '.'),
-        types: types.replace('./dist', '.'),
-        exports,
-        sideEffects,
-        author,
-        license,
-        dependencies,
-      },
-      null,
-      2,
-    )
-  }),
+      dependencies,
+    },
+    null,
+    2,
+  ),
 )
-
-execSync(`pnpm tsc -p tsconfig.build.json`, {
-  stdio: 'inherit',
-})
