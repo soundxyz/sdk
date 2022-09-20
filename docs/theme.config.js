4c8bd1657e5547694ede71ecce8a32222f11f8c8
Pablo Sáez <pablosaez1995@gmail.com>
2022-09-20 02:57:23 -0300

    docs (#95)
    * docs
    
    * meta
    
    * introduction
    
    * react 17
    
    * docs
    
    * soundCreatorAddress
    
    * Remove not-needed audioFromTrack in-between object of editionInfo.trackAudio

--- docs/theme.config.js
+++ docs/theme.config.js
@@ -4,10 +4,10 @@
  */
 export default {
   banner: {
-    key: 'Sound.xyz SDK',
-    text: 'Sound.xyz SDK',
+    key: 'Sound.xyz Docs',
+    text: 'Sound.xyz Docs',
   },
-  logo: 'Sound.xyz SDK',
+  logo: 'Sound.xyz Docs',
   titleSuffix: ' \u2013 Sound.xyz',
   docsRepositoryBase: 'https://github.com/soundxyz/sdk/blob/main/docs',
   editLink: {
@@ -19,21 +19,18 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Sound.xyz SDK" />
+      <meta name="description" content="Sound.xyz Docs" />
       <meta name="og:description" content="Sound.xyz SDK" />
-      <meta name="twitter:card" content="summary_large_image" />
-      {/* <meta name="twitter:image" content="https://nextra.vercel.app/og.png" /> */}
       <meta name="twitter:site:domain" content="www.sound.xyz" />
       <meta name="twitter:url" content="https://www.sound.xyz" />
       <meta name="og:title" content="Sound.xyz SDK" />
-      {/* <meta name="og:image" content="https://nextra.vercel.app/og.png" /> */}
-      <meta name="apple-mobile-web-app-title" content="Sound.xyz SDK" />
-      {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
-      <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
+      <meta name="apple-mobile-web-app-title" content="Sound.xyz Docs" />
+
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" /> */}
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
     </>
   ),
   chat: {
