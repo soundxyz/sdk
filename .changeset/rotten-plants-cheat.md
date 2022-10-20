dfeb644ad11c2ad4154aabc46c653af5cf39c890
Pablo Sáez <pablosaez1995@gmail.com>
2022-10-20 01:05:51 -0300

    Target ES2019, transpiling "?."

---
+++ .changeset/rotten-plants-cheat.md
@@ -0,0 +1,5 @@
+---
+'@soundxyz/sdk': patch
+---
+
+Target ES2019, transpiling "?."
--- packages/sdk/build.mjs
+++ packages/sdk/build.mjs
@@ -22,7 +22,7 @@ const [pkgString] = await Promise.all([
     clean: true,
     format: 'interop',
     outDir: 'dist',
-    target: 'es2020',
+    target: 'es2019',
     sourcemap: false,
   }),
   copyFile('README.md', 'dist/README.md'),
