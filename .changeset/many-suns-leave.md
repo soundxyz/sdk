129ecab1066c249425dc9bfaa9c8159e3bddb607
Pablo Sáez <pablosaez1995@gmail.com>
2022-11-03 18:02:50 -0700

    Composable mint schedules functions

---
+++ .changeset/many-suns-leave.md
@@ -0,0 +1,5 @@
+---
+'@soundxyz/sdk': minor
+---
+
+Composable mint schedules functions with "contractRegisteredMinters","contractMinterMintIds" and "contractMintSchedules"
--- packages/sdk/src/client.ts
+++ packages/sdk/src/client.ts
@@ -64,6 +64,9 @@ export function SoundClient({
     networkChainMatches,
     numberOfTokensOwned,
     getMerkleProof,
+    contractRegisteredMinters,
+    contractMinterMintIds,
+    contractMintSchedules,
   }
 
   const IdempotentCache: Record<string, unknown> = {}
@@ -120,7 +123,25 @@ export function SoundClient({
   }): Promise<MintSchedule[]> {
     await _requireValidSoundEdition({ editionAddress })
 
-    return _allMintSchedules({ editionAddress, fromBlockOrBlockHash })
+    const minterAddresses = await contractRegisteredMinters({
+      editionAddress,
+      fromBlockOrBlockHash,
+    })
+
+    const scheduleIds = await Promise.all(
+      minterAddresses.map(async (minterAddress) => {
+        return {
+          minterAddress,
+          mintIds: await contractMinterMintIds({
+            editionAddress,
+            minterAddress,
+            fromBlockOrBlockHash,
+          }),
+        }
+      }),
+    )
+
+    return contractMintSchedules({ editionAddress, scheduleIds })
   }
 
   // Active minter schedules for a given edition
@@ -135,10 +156,10 @@ export function SoundClient({
   }): Promise<MintSchedule[]> {
     await _requireValidSoundEdition({ editionAddress })
 
-    const mintSchedules = await _allMintSchedules({ editionAddress, fromBlockOrBlockHash })
+    const allMintSchedules = await mintSchedules({ editionAddress, fromBlockOrBlockHash })
 
     // Filter mints that are live during the given timestamp
-    return mintSchedules
+    return allMintSchedules
       .filter((mintSchedule) => {
         return mintSchedule.startTime <= timestamp && mintSchedule.endTime > timestamp && !mintSchedule.mintPaused
       })
@@ -496,12 +517,8 @@ export function SoundClient({
     return networkChain === chainId
   }
 
-  /*********************************************************
-                  INTERNAL FUNCTIONS
- ********************************************************/
-
   // Addresses with MINTER_ROLE for a given edition
-  async function _registeredMinters({
+  async function contractRegisteredMinters({
     editionAddress,
     fromBlockOrBlockHash,
   }: {
@@ -541,8 +558,7 @@ export function SoundClient({
     return Array.from(allMinters)
   }
 
-  // Minting information from a minting contract for a given edition
-  async function _mintInfosFromMinter({
+  async function contractMinterMintIds({
     editionAddress,
     minterAddress,
     fromBlockOrBlockHash,
@@ -550,14 +566,50 @@ export function SoundClient({
     editionAddress: string
     minterAddress: string
     fromBlockOrBlockHash: BlockOrBlockHash | undefined
-  }): Promise<MintSchedule[]> {
+  }) {
     const { signerOrProvider } = await _requireSignerOrProvider()
 
     // Query MintConfigCreated event
     const minterContract = IMinterModule__factory.connect(minterAddress, signerOrProvider)
     const filter = minterContract.filters.MintConfigCreated(editionAddress)
     const mintScheduleConfigEvents = await minterContract.queryFilter(filter, fromBlockOrBlockHash)
-    const mintIds = mintScheduleConfigEvents.map((event) => event.args.mintId)
+    return mintScheduleConfigEvents.map((event) => event.args.mintId.toNumber())
+  }
+
+  async function contractMintSchedules({
+    editionAddress,
+    scheduleIds,
+  }: {
+    editionAddress: string
+    scheduleIds: {
+      minterAddress: string
+      mintIds: number[]
+    }[]
+  }): Promise<MintSchedule[]> {
+    const mintSchedules = await Promise.all(
+      scheduleIds.map(async ({ minterAddress, mintIds }) =>
+        _mintInfosFromMinter({ editionAddress, minterAddress, mintIds }),
+      ),
+    )
+
+    return mintSchedules.flat().sort((a, b) => a.startTime - b.startTime)
+  }
+
+  /*********************************************************
+                  INTERNAL FUNCTIONS
+ ********************************************************/
+
+  // Minting information from a minting contract for a given edition
+  async function _mintInfosFromMinter({
+    editionAddress,
+    minterAddress,
+    mintIds,
+  }: {
+    editionAddress: string
+    minterAddress: string
+    mintIds: number[]
+  }): Promise<MintSchedule[]> {
+    const { signerOrProvider } = await _requireSignerOrProvider()
 
     return Promise.all(
       mintIds.map(async (mintId) => {
@@ -571,7 +623,7 @@ export function SoundClient({
             return {
               mintType: 'RangeEdition',
               interfaceId,
-              mintId: mintId.toNumber(),
+              mintId,
               editionAddress,
               minterAddress,
               startTime: mintSchedule.startTime,
@@ -594,7 +646,7 @@ export function SoundClient({
             const mintSchedule = await minterContract.mintInfo(editionAddress, mintId)
             return {
               mintType: 'MerkleDrop',
-              mintId: mintId.toNumber(),
+              mintId,
               merkleRoot: mintSchedule.merkleRootHash,
               editionAddress,
               minterAddress,
@@ -613,24 +665,6 @@ export function SoundClient({
         }
       }),
     )
-  }
-
-  async function _allMintSchedules({
-    editionAddress,
-    fromBlockOrBlockHash,
-  }: {
-    editionAddress: string
-    fromBlockOrBlockHash: BlockOrBlockHash | undefined
-  }): Promise<MintSchedule[]> {
-    const registeredMinters = await _registeredMinters({ editionAddress, fromBlockOrBlockHash })
-
-    const mintSchedules = await Promise.all(
-      registeredMinters.map(async (minterAddress) =>
-        _mintInfosFromMinter({ editionAddress, minterAddress, fromBlockOrBlockHash }),
-      ),
-    )
-
-    return mintSchedules.flat().sort((a, b) => a.startTime - b.startTime)
   }
 
   async function _requireSigner(): Promise<{ signer: Signer; userAddress: string }> {
