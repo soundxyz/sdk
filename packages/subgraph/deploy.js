b2b267dcd19f0c70bfac8e8a87285df235df8bc0
Saihajpreet Singh <saihajpreet.singh@gmail.com>
2022-11-10 19:20:00 -0500

    deploy script

---
+++ packages/subgraph/deploy.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const path = require('path')
+const yaml = require('js-yaml')
+
+const subgraphYaml = yaml.load(fs.readFileSync('subgraph.yaml', 'utf8'))
+const DEPLOY_NETWORK = process.env['DEPLOY_NETWORK'] || 'mainnet'
+
+const network = fs.readFileSync(path.join(__dirname, 'networks', `${process.env['ENV_NAME']}.json`), 'utf8')
+const parsedNetworkFile = JSON.parse(network)
+
+subgraphYaml.dataSources[0].network = DEPLOY_NETWORK
+subgraphYaml.dataSources[0].source.address = parsedNetworkFile[DEPLOY_NETWORK]['SoundCreatorV1']['address']
+subgraphYaml.dataSources[0].source.startBlock = parsedNetworkFile[DEPLOY_NETWORK]['SoundCreatorV1']['startBlock']
+
+subgraphYaml.dataSources[1].network = DEPLOY_NETWORK
+subgraphYaml.dataSources[1].source.address = parsedNetworkFile[DEPLOY_NETWORK]['MerkleDropMinter']['address']
+subgraphYaml.dataSources[1].source.startBlock = parsedNetworkFile[DEPLOY_NETWORK]['MerkleDropMinter']['startBlock']
+
+subgraphYaml.dataSources[2].network = DEPLOY_NETWORK
+subgraphYaml.dataSources[2].source.address = parsedNetworkFile[DEPLOY_NETWORK]['RangeEditionMinter']['address']
+subgraphYaml.dataSources[2].source.startBlock = parsedNetworkFile[DEPLOY_NETWORK]['RangeEditionMinter']['startBlock']
+
+subgraphYaml.templates[0].network = DEPLOY_NETWORK
+
+const newSubgraphYaml = yaml.dump(subgraphYaml)
+fs.writeFileSync('subgraph.yaml', newSubgraphYaml)
