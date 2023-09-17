edb62290585fe1bfd0f266842e1ea58d4a442b92
Vignesh Hirudayakanth <vigneshkanth@gmail.com>
2023-09-16 20:24:12 -0700

    add public actions

---
+++ packages/tiered-sdk-viem/src/actions/public/getEditionContractInfo.ts
@@ -0,0 +1,59 @@
+import type { Chain, PublicClient, Transport } from 'viem'
+import { Address } from 'viem'
+import { SoundEditionV2Config } from '../../abi/sound-edition-v2'
+
+export interface GetEditionContractInfoParams {
+  editionAddress: Address
+}
+
+export interface GetEditionContractInfoReturnType {
+  baseURI: string
+  contractURI: string
+  name: string
+  symbol: string
+  fundingRecipient: Address
+  metadataModule: Address
+  isMetadataFrozen: boolean
+  isCreateTierFrozen: boolean
+  royaltyBPS: number
+  nextTokenId: bigint
+  totalBurned: bigint
+  totalMinted: bigint
+  totalSupply: bigint
+  tierInfo: readonly {
+    tier: number
+    maxMintable: number | ((unixTimestamp?: number) => number)
+    maxMintableLower: number
+    maxMintableUpper: number
+    cutoffTime: number
+    minted: number
+    mintRandomness: bigint
+    mintConcluded: boolean
+    mintRandomnessEnabled: boolean
+    isFrozen: boolean
+  }[]
+}
+
+export async function getEditionContractInfo<TChain extends Chain | undefined>(
+  client: PublicClient<Transport, TChain>,
+  { editionAddress }: GetEditionContractInfoParams,
+): Promise<GetEditionContractInfoReturnType> {
+  const data = await client.readContract({
+    abi: SoundEditionV2Config.abi,
+    address: editionAddress,
+    functionName: 'editionInfo',
+  })
+
+  return {
+    ...data,
+    tierInfo: data.tierInfo.map((tierInfo) => ({
+      ...tierInfo,
+      maxMintable:
+        tierInfo.maxMintableLower === tierInfo.maxMintableUpper
+          ? tierInfo.maxMintableLower
+          : (unixTimestamp: number | undefined = Date.now() / 1000) =>
+              // if before the cutoff time, use the upper limit, otherwise use the lower limit
+              unixTimestamp < tierInfo.cutoffTime ? tierInfo.maxMintableUpper : tierInfo.maxMintableLower,
+    })),
+  }
+}
---
+++ packages/tiered-sdk-viem/src/actions/public/getMintingSchedules.ts
@@ -0,0 +1,73 @@
+import type { Chain, Hex, PublicClient, Transport } from 'viem'
+import { Address } from 'viem'
+import { SuperMinterV1Config } from '../../abi/super-minter-v1'
+
+export interface GetMintingSchedulesParams {
+  editionAddress: Address
+  superMinterAddress: Address
+  unixTimestamp?: number
+}
+
+export interface ScheduleBase {
+  edition: Address
+  tier: number
+  scheduleNum: number
+  platform: Address
+  price: bigint
+  startTime: number
+  endTime: number
+  maxMintablePerAccount: number
+  maxMintable: number
+  minted: number
+  affiliateFeeBPS: number
+  paused: boolean
+  hasMints: boolean
+  affiliateMerkleRoot: Hex
+}
+export interface DefaultSchedule extends ScheduleBase {
+  type: 'DEFAULT'
+}
+export interface MerkleSchedule extends ScheduleBase {
+  type: 'VERIFY_MERKLE'
+  merkleRoot: Hex
+}
+export interface SignatureSchedules extends ScheduleBase {
+  type: 'VERIFY_SIGNATURE'
+  signer: Address
+  usePlatformSigner: boolean
+}
+
+export type SuperMinterSchedule = DefaultSchedule | MerkleSchedule | SignatureSchedules
+
+export interface GetMintingSchedulesReturnType {
+  schedules: readonly SuperMinterSchedule[]
+  activeSchedules: readonly SuperMinterSchedule[]
+}
+
+export async function getMintingSchedules<TChain extends Chain | undefined>(
+  client: PublicClient<Transport, TChain>,
+  { editionAddress, superMinterAddress, unixTimestamp = Date.now() / 1000 }: GetMintingSchedulesParams,
+): Promise<GetMintingSchedulesReturnType> {
+  const schedules: SuperMinterSchedule[] = await client
+    .readContract({
+      abi: SuperMinterV1Config.abi,
+      address: superMinterAddress,
+      functionName: 'mintInfoList',
+      args: [editionAddress],
+    })
+    .then((schedules) =>
+      schedules.map((schedule) => ({
+        ...schedule,
+        type: schedule.mode === 0 ? 'DEFAULT' : schedule.mode === 1 ? 'VERIFY_MERKLE' : 'VERIFY_SIGNATURE',
+      })),
+    )
+
+  const activeSchedules = schedules.filter((mintSchedule) => {
+    return mintSchedule.startTime <= unixTimestamp && mintSchedule.endTime > unixTimestamp && !mintSchedule.paused
+  })
+
+  return {
+    schedules,
+    activeSchedules,
+  }
+}
