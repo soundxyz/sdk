aaf93955bed8260faeb3cbd83a94b2e97b8bcbf6
Pablo Sáez <pablosaez1995@gmail.com>
2023-10-17 11:04:48 -0400

    remove hardhat

--- packages/old-sdk/test/test-constants.ts
+++
@@ -1,8 +0,0 @@
-import { getSaltAsBytes32 } from '../src/utils/helpers'
-
-export const DEFAULT_SALT = getSaltAsBytes32(12345678)
-export const SOUND_FEE = 0
-export const ONE_HOUR = 3600
-export const PRICE = 420420420
-export const BAD_ADDRESS = 'not an address'
-export const EDITION_MAX = 100
