30b23c9bda6f512c873834a0f97d465b0051fd6c
Pablo Sáez <pablosaez1995@gmail.com>
2023-10-17 18:01:03 -0400

    fix icon

--- examples/nextjs/src/components/walletInput.tsx
+++ examples/nextjs/src/components/walletInput.tsx
@@ -1,5 +1,7 @@
 import { setWalletPrivateKey, useWalletPrivateKey } from '@/context/wallet'
 import React, { useState } from 'react'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export function WalletPrivateKeyInput() {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false)
@@ -37,21 +39,7 @@ export function WalletPrivateKeyInput() {
             fill="currentColor"
             aria-hidden="true"
           >
-            {isPasswordVisible ? (
-              // Eye Slash Icon
-              <path
-                fillRule="evenodd"
-                d="M2.003 5.884l1.215 1.215a8 8 0 0111.348 11.348l1.215 1.215a1 1 0 101.414-1.414l-15-15a1 1 0 00-1.414 1.414l.622.622A10 10 0 000 10c1.528 2.977 4.356 5.328 7.535 6.292a2 2 0 11-1.23 1.074A9.997 9.997 0 012.003 5.884zM4.566 4.292a2 2 0 012.977-2.977l1.263 1.263a4 4 0 015.096 5.096l1.263 1.263a2 2 0 11-2.977 2.977l-7.72-7.72zM7 10a3 3 0 00-.072.876l.895.895a3 3 0 00.072-1.77l-.895-.896z"
-                clipRule="evenodd"
-              />
-            ) : (
-              // Eye Icon
-              <path
-                fillRule="evenodd"
-                d="M10 2a8 8 0 016.154 13.881l-1.581-1.582a5 5 0 00-6.987-6.987L2.119 4.267A8 8 0 0110 2zm0 3a3 3 0 012.083 5.122l-4.201 4.201A3 3 0 0110 5zm0 11a8 8 0 01-5.546-2.148l1.49-1.49a5 5 0 007.11-7.11l1.548-1.548A8 8 0 0110 16z"
-                clipRule="evenodd"
-              />
-            )}
+            {isPasswordVisible ? <FontAwesomeIcon icon={faEye} /> : <FontAwesomeIcon icon={faEyeSlash} />}
           </svg>
         </div>
       </div>
