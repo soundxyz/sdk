19dba6ecb3041f46f44cd6dbf9c25065da917642
Pablo Sáez <pablosaez1995@gmail.com>
2023-10-17 16:50:18 -0400

    pull env

--- examples/nextjs/next.config.js
+++ examples/nextjs/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    PUBLIC_ALCHEMY_KEY: process.env.PUBLIC_ALCHEMY_KEY,
+    PUBLIC_WALLET_PRIVATE_KEY: process.env.PUBLIC_WALLET_PRIVATE_KEY,
+  },
+}
 
 module.exports = nextConfig
