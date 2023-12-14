e4a02f7ef8b61255168b63600cb570ebcb164d0e
Vignesh Hirudayakanth <vigneshkanth@gmail.com>
2023-12-14 11:27:15 -0800

    handle null case

--- packages/sdk/src/utils/cache-utils.ts
+++ packages/sdk/src/utils/cache-utils.ts
@@ -2,11 +2,12 @@ export class CacheUtils {
   private static cache = new Map<string, unknown>()
 
   public static async getOrSetCache<T>(key: string, fetchData: () => Promise<T>): Promise<T> {
-    let data = this.cache.get(key)
-    if (!data) {
-      data = await fetchData()
-      this.cache.set(key, data)
+    if (this.cache.has(key)) {
+      return this.cache.get(key) as T
     }
-    return data as T
+    const data = await fetchData()
+    this.cache.set(key, data)
+
+    return data
   }
 }
