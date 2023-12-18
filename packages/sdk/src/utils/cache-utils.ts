c53c0391278f15122d8e0f4d49d297bb1a35b311
Pablo Sáez <pablosaez1995@gmail.com>
2023-12-18 12:36:57 -0500

    save promise wrapper

--- packages/sdk/src/utils/cache-utils.ts
+++ packages/sdk/src/utils/cache-utils.ts
@@ -1,13 +1,15 @@
 export class CacheUtils {
-  private static cache = new Map<string, unknown>()
+  private static cache = new Map<string, Promise<unknown>>()
 
   public static async getOrSetCache<T>(key: string, fetchData: () => Promise<T>): Promise<T> {
     if (this.cache.has(key)) {
-      return this.cache.get(key) as T
+      return this.cache.get(key) as Promise<T>
     }
-    const data = await fetchData()
-    this.cache.set(key, data)
 
-    return data
+    const promise = fetchData()
+
+    this.cache.set(key, promise)
+
+    return promise
   }
 }
